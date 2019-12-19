152aa23d3ffc70c326b32781e50c15ed915b1401
Alberto Basalo <albertobasalo@hotmail.com>
2019-12-19 19:22:31 +0100

    if(isPlatformBrowser(this.platformId))

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
-import { ApiService } from "../api.service";
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from '../api.service';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   countries$: Observable<any[]>;
 
-  constructor(private api: ApiService) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
+    private api: ApiService
+  ) {}
 
   ngOnInit() {
     this.countries$ = this.api.getAllCountries$();
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('EscuelaIT', 'Angular-Universal');
+    } else {
+      console.log('Estamos en el servidor');
+    }
   }
 }
